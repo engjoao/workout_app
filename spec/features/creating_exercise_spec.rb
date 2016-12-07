28de8e452e59b47e1ccd41f9633c5dfe4d7f26a1
engjoao <aquinosilva@gmail.com>
2016-12-07 22:06:17 +0000

    Add charts to workouts

--- spec/features/creating_exercise_spec.rb
+++ spec/features/creating_exercise_spec.rb
@@ -3,8 +3,8 @@ require "rails_helper"
 RSpec.feature "Creating Exercise" do
 
   before do
-      @john = User.create!(email: "john@example.com", password: "password")
-      login_as(@john)
+    @john = User.create!(email: "john@example.com", password: "password")
+    login_as(@john)
   end
   
   scenario "with valid inputs" do
