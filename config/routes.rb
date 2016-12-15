40aa4372c0d234bc18c65cd146331267594fdb1c
engjoao <aquinosilva@gmail.com>
2016-12-15 16:28:33 +0000

    Implement searching

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,14 @@ Rails.application.routes.draw do
   resources :users do
     resources :exercises
   end
-
+  
+  resource :dashboard, only: [:index] do
+    collection do
+      post :search, to: 'dashboard#search'
+    end
+  end
+  
+  
   root to: 'dashboard#index'
+
 end
